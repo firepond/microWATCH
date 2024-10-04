690f9e1707cce545dd33b28e0167470114a8b838
firepond <915644848@qq.com>
2024-10-04 11:23:26 +0100

    Update.

--- src/x_watch.py
+++ src/x_watch.py
@@ -157,6 +157,36 @@ class WATCH:
 
 
 def main():
+    dummy_data = np.array(
+        [
+            [1, 2],
+            [2, 3],
+            [3, 4],
+            [4, 5],
+            [5, 6],
+            [1, 2],
+            [2, 3],
+            [3, 4],
+            [4, 5],
+            [5, 6],
+            [1000, 1000],
+            [5000, 5000],
+            [10000, 10000],
+            [1000, 1000],
+            [5000, 5000],
+            [10000, 10000],
+            [1, 1],
+            [1, 1],
+            [1, 1],
+            [1, 1],
+            [1, 1],
+            [1, 1],
+        ]
+    )
+
+    watch = WATCH()
+    locations = watch.detect(dummy_data)
+
     dataset_name = "apple"
     dataset_prefix = "./datasets/"
     dataset_loc = dataset_prefix + "/" + dataset_name + ".json"
