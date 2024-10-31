ede9cdda1c3f2ee08649da1823ea71c02437a3c2
firepond <915644848@qq.com>
2024-10-31 12:10:00 +0000

    Add different metric and post process for x_watch.

--- src/x_watch.py
+++ src/x_watch.py
@@ -1,85 +1,12 @@
 import math
+import os
 import numpy as np
 import ot
 
-from load_dataset import TimeSeries
-import argparse
+import accuracy
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(description="Wrapperfor None-detector")
-    parser.add_argument("-i", "--input", help="input datasets name", required=True)
-    parser.add_argument("-o", "--output", help="path to the output file")
-    return parser.parse_args()
-
-
-def find_unique_sub_arrays(arr):
-    """
-    Find unique sub-arrays and their counts in an n-dimensional NumPy array.
-
-    Args:
-        arr (numpy.ndarray): Input n-dimensional array.
-
-    Returns:
-        tuple: A tuple containing:
-            - unique_sub_arrays (numpy.ndarray): Array of unique sub-arrays.
-            - counts (numpy.ndarray): Array of counts corresponding to unique sub-arrays.
-    """
-    # Get the number of dimensions in the input array
-    num_dims = arr.ndim
-
-    # Initialize a dictionary to track unique sub-arrays and their frequencies
-    unique_sub_arrays = {}
-
-    # Calculate the shape for slicing
-    shape = arr.shape
-
-    if num_dims == 1:
-        unique_sub_arrays = np.unique(arr)
-        counts = np.unique(arr, return_counts=True)
-        return unique_sub_arrays, counts[1]
-
-    # Generate slices based on the number of dimensions
-    if num_dims == 2:
-        # For 2D arrays, loop through all possible sub-arrays
-        for i in range(shape[0]):
-            sub_array = arr[i, :]
-            sub_array_tuple = tuple(map(tuple, [sub_array]))  # Make it hashable
-            unique_sub_arrays[sub_array_tuple] = (
-                unique_sub_arrays.get(sub_array_tuple, 0) + 1
-            )
-
-    elif num_dims == 3:
-        # For 3D arrays, loop through all possible 2D slices
-        for i in range(shape[0]):
-            sub_array = arr[i, :, :]
-            sub_array_tuple = tuple(map(tuple, sub_array))  # Make it hashable
-            unique_sub_arrays[sub_array_tuple] = (
-                unique_sub_arrays.get(sub_array_tuple, 0) + 1
-            )
-
-    else:
-        # For higher dimensions, loop through the first dimension and treat the rest as a sub-array
-        for i in range(shape[0]):
-            sub_array = arr[i, ...]  # Use Ellipsis for higher dimensions
-            sub_array_tuple = tuple(
-                map(tuple, map(np.ravel, sub_array))
-            )  # Make it hashable
-            unique_sub_arrays[sub_array_tuple] = (
-                unique_sub_arrays.get(sub_array_tuple, 0) + 1
-            )
-
-    # Convert unique sub-arrays and their counts to numpy arrays
-    sub_arrays_list = np.array(
-        [np.array(sub_array) for sub_array in unique_sub_arrays.keys()]
-    )
-    counts_list = np.array(list(unique_sub_arrays.values()))
-    if num_dims == 2:
-        sub_arrays_list = sub_arrays_list[:, 0, :]
-    return sub_arrays_list, counts_list
-
-
-def wassertein(sample, dist):
+def wassertein(sample, dist, metric="euclidean", post_process="null"):
     """
     Calculate the Wasserstein distance between two distributions
 
@@ -103,13 +30,21 @@ def wassertein(sample, dist):
     dist_dist = dist_freq / dist.shape[0]
 
     # loss matrix
-    loss_matrix = ot.dist(sample_list, dist_list, metric="euclidean")
+    loss_matrix = ot.dist(sample_list, dist_list, metric)
 
     ot_dis = ot.emd2(sample_dist, dist_dist, loss_matrix)
-    return ot_dis
+    # check name of post_process
+    if post_process == "sqrt":
+        return np.sqrt(ot_dis)
+    elif post_process == "square":
+        return ot_dis**2
+    else:
+        return ot_dis
 
 
 def iterate_batches(points, batch_size):
+    # this function is used to iterate over the data points in batches
+
     samples_number = math.ceil(points.shape[0] / batch_size)
     for sample_id in range(0, samples_number):
         sample = points[sample_id * batch_size : (sample_id + 1) * batch_size]
@@ -123,19 +58,21 @@ class WATCH:
         new_dist_buffer_size=16,
         batch_size=3,
         max_dist_size=100,
-        DEBUG=False,
     ):
         self.threshold_ratio = threshold
         self.max_dist_size = max_dist_size
         self.new_dist_buffer_size = new_dist_buffer_size
         self.batch_size = batch_size
+        self.metric = "euclidean"
+        self.post_process = "null"
+
         self.is_creating_new_dist = True
+
         self.dist = []
         self.dist_values = []
         self.locations = []
-        self.DEBUG = DEBUG
-        if self.DEBUG:
-            self.values = []
+
+        self.values = []
 
     def detect(self, data):
         for batch_id, batch in enumerate(
@@ -146,13 +83,20 @@ class WATCH:
                 if len(self.dist) >= self.new_dist_buffer_size:
                     self.is_creating_new_dist = False
                     values = [
-                        wassertein(np.array(s), np.array(self.dist))
+                        wassertein(
+                            np.array(s),
+                            np.array(self.dist),
+                            self.metric,
+                            self.post_process,
+                        )
                         for s in iterate_batches(np.array(self.dist), self.batch_size)
                     ]
                     self.threshold = np.max(values) * self.threshold_ratio
                     # print(self.threshold)
             else:
-                value = wassertein(np.array(batch), np.array(self.dist))
+                value = wassertein(
+                    np.array(batch), np.array(self.dist), self.metric, self.post_process
+                )
 
                 if value > self.threshold:
                     self.locations.append(batch_id * self.batch_size)
@@ -161,7 +105,12 @@ class WATCH:
                 if self.max_dist_size == 0 or len(self.dist) < self.max_dist_size:
                     self.dist.extend(batch)
                     values = [
-                        wassertein(np.array(s), np.array(self.dist))
+                        wassertein(
+                            np.array(s),
+                            np.array(self.dist),
+                            self.metric,
+                            self.post_process,
+                        )
                         for s in iterate_batches(np.array(self.dist), self.batch_size)
                     ]
                     self.threshold = np.max(values) * self.threshold_ratio
@@ -169,59 +118,27 @@ class WATCH:
         return self.locations
 
 
-def dummy_test():
-    dummy_data = np.array(
-        [
-            [1, 2],
-            [2, 3],
-            [3, 4],
-            [4, 5],
-            [5, 6],
-            [1, 2],
-            [2, 3],
-            [3, 4],
-            [4, 5],
-            [5, 6],
-            [1000, 1000],
-            [5000, 5000],
-            [10000, 10000],
-            [1000, 1000],
-            [5000, 5000],
-            [10000, 10000],
-            [1, 1],
-            [1, 1],
-            [1, 1],
-            [1, 1],
-            [1, 1],
-            [1, 1],
-        ]
-    )
-
-    watch = WATCH()
-    locations = watch.detect(dummy_data)
-    print(locations)
-
-
 def main():
-
-    args = parse_args()
-
-    dataset_name = args.input
-    dataset_prefix = "./datasets/"
-    dataset_loc = dataset_prefix + "/" + dataset_name
-    dataset_loc = dataset_name
-    print(dataset_loc)
-    ts = TimeSeries.from_json(dataset_loc)
-
-    data = ts.df.to_numpy()
-    data = data[::, 1::]
-
-    print(data.shape)
-
-    watch = WATCH()
-    locations = watch.detect(data)
-
-    print(locations)
+    dir = "./csv"
+    # for all files in dir test the watch algorithm
+    files = os.listdir(dir)
+    metric = "sqeuclidean"
+    post_process = "sqrt"
+    for file in files:
+        dataset_name = file.split("/")[-1].split(".")[0]
+        data_loc = f"./csv/{file}"
+        print(data_loc)
+        data = np.loadtxt(data_loc, delimiter=",")
+        # convert 1 dimensional data to m * 1 np array
+        if len(data.shape) == 1:
+            data = data.reshape(-1, 1)
+        watch = WATCH()
+        watch.metric = metric
+        watch.post_process = post_process
+        locations = watch.detect(data)
+        print(locations)
+        f1, cover = accuracy.scores(locations, dataset_name, data.shape[0])
+        print(f1, cover)
 
 
 if __name__ == "__main__":
